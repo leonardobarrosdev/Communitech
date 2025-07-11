175ae6a7f8ffef300d876d6b9518e298b58cd5d0
Leonardo <leonardobarrosdw@gmail.com>
2025-07-11 16:23:53 -0300

    feat: AuthTokenSerializer

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,8 +1,47 @@
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
 from django.utils.translation import gettext as _
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from apps.user.models import Profile
+
+
+class AuthTokenSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(
+        label=_("email"), write_only=True
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label=_("Token"), read_only=True
+    )
+
+    class Meta:
+        model = Profile
+        fields = ["email", "password", "token"]
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(
+                request=self.context.get('request'),
+                email=email,
+                password=password
+            )
+            if not user:
+                msg = _('Unable to log in with provided credentials.')
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = _('Must include "email" and "password".')
+            raise serializers.ValidationError(msg, code='authorization')
+        attrs['user'] = user
+        return attrs
 
 
 class ProfileSerializer(serializers.ModelSerializer):
