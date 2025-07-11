616029d08a01182e6d03ef188ca54e5643de86c6
Leonardo <leonardobarrosdw@gmail.com>
2025-07-11 16:22:26 -0300

    feat: login and logout added

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path, include, re_path
-from .views import RegisterAPIView, UpdateProfileAPIView, UpdateAuthAPIView
+from knox.views import LogoutView
+from .views import (
+    RegisterAPIView,
+    LoginView,
+    UpdateProfileAPIView,
+    UpdateAuthAPIView
+)
 
 
 app_name = "profile"
 
 urlpatterns = [
     path("auth/register/", RegisterAPIView.as_view(), name="register"),
+    path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
     path("profiles/<int:id>/update/", UpdateProfileAPIView.as_view(), name="update"),
     path(
         "profiles/<int:id>/update-auth/",
