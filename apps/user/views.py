a9492f1af343b3a7de7e556f83e5ac0eff9e6c99
Leonardo <leonardobarrosdw@gmail.com>
2025-07-16 22:57:54 -0300

    feat: LoginView implemented

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,15 +1,15 @@
 from rest_framework import status, generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-# from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.auth import TokenAuthentication
-from knox.models import AuthToken
-from django.contrib.auth import login
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authentication import TokenAuthentication
 from django.template.loader import render_to_string
 from django.utils.translation import gettext as _
-from apps.user.serializers import ProfileSerializer, RegisterSerializer
-from apps.user.utils import Util, CsrfExemptSessionAuthentication
+from apps.user.utils import Util
 from apps.user.serializers import (
+    ProfileSerializer,
+    RegisterSerializer,
     AuthTokenSerializer,
     UpdateProfileSerializer,
     UpdateAuthSerializer
@@ -18,7 +18,6 @@ from apps.user.models import Profile
 
 
 class RegisterAPIView(generics.CreateAPIView):
-    authentication_classes = [CsrfExemptSessionAuthentication]
     queryset = Profile.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
@@ -30,7 +29,7 @@ class RegisterAPIView(generics.CreateAPIView):
                 {"error": "Invalid fields."}, status=status.HTTP_400_BAD_REQUEST
             )
         profile = serializer.save()
-        token_object, token = AuthToken.objects.create(profile)
+        token = Token.objects.create(user=profile)
         email_body = render_to_string("emails/welcome.txt", {"user": profile})
         data = {
             "email_subject": _(
@@ -45,26 +44,32 @@ class RegisterAPIView(generics.CreateAPIView):
                 "user": ProfileSerializer(
                     profile, context=self.get_serializer_context()
                 ).data,
-                "token": token,
+                "token": token.key,
             },
             status=status.HTTP_201_CREATED,
         )
 
 
-class LoginView(generics.GenericAPIView):
-    authentication_classes = [CsrfExemptSessionAuthentication]
+class LoginView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
-    permission_classes = (AllowAny,)
 
-    def post(self, request, format=None):
-        serializer = self.get_serializer(data=request.data)
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
         serializer.is_valid(raise_exception=True)
         profile = serializer.validated_data['user']
-        login(request, profile)
-        response = super(LoginView, self).post(request, format=None)
-        profile_serialized = ProfileSerializer(profile)
-        response.data['user'] = profile_serialized.data
-        return response
+        token, created = Token.objects.get_or_create(user=profile)
+        return Response({
+            'token': token.key,
+            'user': {
+                'id': profile.pk,
+                'first_name': profile.first_name,
+                'username': profile.username,
+                'email': profile.email
+            }
+        })
 
 
 class UpdateProfileAPIView(generics.UpdateAPIView):
